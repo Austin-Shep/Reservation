1ef292d1d77bc6d1d16c292334f1562a4376808e
JecoLantern <jbayonke@hotmail.com>
2018-11-07 19:13:48 -0800

    html route module export

---
+++ app/routes/html-route.js
@@ -0,0 +1,15 @@
+var path = require("path");
+
+module.exports = function(app) {
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "home.html"));
+    });
+     
+    app.get("/view", function(req, res) {
+      res.sendFile(path.join(__dirname,"view.html"));
+    });
+     
+    app.get("/make", function(req, res) {
+      res.sendFile(path.join(__dirname,"make.html"));
+    });
+}
