ac1ee996ab6eaab3356a3f8fdb18856286cdf4fe
JecoLantern <jbayonke@hotmail.com>
2018-11-07 19:30:40 -0800

    table and waitinglist data js

---
+++ data/table-data.js
@@ -0,0 +1,8 @@
+var tableArray = [{
+    customerName: "Llewellyn",
+    customerTime: "0700",
+    customerParty: "5",
+    phoneNumber: "628-999-0000"
+}];
+
+module.exports = tableArray;
--- server.js
+++ server.js
@@ -1,1 +1,14 @@
+var express = require("express");
+var path = require("path");
 
+var app = express();
+var PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require('./app/routes/html-route.js')(app);
+
+app.listen(PORT, function() {
+	console.log("App listening on PORT: " + PORT);
+});
