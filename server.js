2b33e4dc03a2e7393f1eab7b53c0251397ee7667
Austin.Shepherd <Austin.Shepherd.96@gmail.com>
2018-11-07 19:20:38 -0800

    server added

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
+//server stuff
+var express = require("express");
+var routes = require("./app/routes/html-route.js")(app);
 
+var app = express();
+var PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(PORT);
+console.log('Server started! At http://localhost:' + PORT);
