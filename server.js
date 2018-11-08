3e61a1452ba060f77e6b6e32a1914851d35a18b3
daniel <dannygrams@yahoo.com>
2018-11-07 19:38:03 -0800

    added api get post

--- server.js
+++ server.js
@@ -1,12 +1,53 @@
-//server stuff
+// Dependencies
+// =============================================================
 var express = require("express");
-var routes = require("./app/routes/html-route.js")(app);
+var path = require("path");
 
+// Sets up the Express App
+// =============================================================
 var app = express();
 var PORT = 3000;
 
+// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.listen(PORT);
-console.log('Server started! At http://localhost:' + PORT);
+
+var rsv = [];
+
+
+// Displays a single character, or returns false
+app.get("/api/rsv/:person", function(req, res) {
+    var chosen = req.params.person;
+  
+    console.log(chosen);
+  
+    for (var i = 0; i < rsv.length; i++) {
+      if (chosen === rsv[i].name) {
+        return res.json(rsv[i]);
+      }
+    }
+  
+    return res.json(false);
+  });
+  
+
+app.post("/rsv" , function(req,res){
+
+    var newRsv = req.body;
+
+    newRsv.name.replace(/\s+/g, "").toLowerCase();
+
+    console.log(newRsv);
+
+    rsv.push(newRsv);
+
+    res.json(newRsv);
+})
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
