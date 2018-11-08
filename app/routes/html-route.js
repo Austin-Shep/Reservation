7704d696fda955de0f47213b9ae009c708cd3409
Austin.Shepherd <Austin.Shepherd.96@gmail.com>
2018-11-07 20:16:26 -0800

    rout fix

--- app/routes/html-route.js
+++ app/routes/html-route.js
@@ -1,15 +1,45 @@
 var path = require("path");
+var express = require("express")
+var router = express.Router();
 
-module.exports = function(app) {
-    app.get("/", function(req, res) {
-        res.sendFile(path.join(__dirname, "home.html"));
-    });
-     
-    app.get("/view", function(req, res) {
-      res.sendFile(path.join(__dirname,"view.html"));
-    });
-     
-    app.get("/make", function(req, res) {
-      res.sendFile(path.join(__dirname,"make.html"));
-    });
-}
+router.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "home.html"));
+});
+
+router.get("/view", function(req, res) {
+  res.sendFile(path.join(__dirname,"view.html"));
+});
+
+router.get("/make", function(req, res) {
+  res.sendFile(path.join(__dirname,"make.html"));
+});
+
+// Displays a single character, or returns false
+router.get("/api/rsv/:person", function(req, res) {
+  var chosen = req.params.person;
+
+  console.log(chosen);
+
+  for (var i = 0; i < rsv.length; i++) {
+    if (chosen === rsv[i].name) {
+      return res.json(rsv[i]);
+    }
+  }
+
+  return res.json(false);
+});
+
+router.post("/rsv" , function(req,res){
+
+  var newRsv = req.body;
+
+  newRsv.name.replace(/\s+/g, "").toLowerCase();
+
+  console.log(newRsv);
+
+  rsv.push(newRsv);
+
+  res.json(newRsv);
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 // =============================================================
 var express = require("express");
 var path = require("path");
-var routes = require("./app/routes/html-route.js")
+var routes = require("./app/routes/html-route")
 
 // Sets up the Express App
 // =============================================================
@@ -17,36 +17,9 @@ app.use(express.json());
 var rsv = [];
 
 
-// Displays a single character, or returns false
-app.get("/api/rsv/:person", function(req, res) {
-    var chosen = req.params.person;
-  
-    console.log(chosen);
-  
-    for (var i = 0; i < rsv.length; i++) {
-      if (chosen === rsv[i].name) {
-        return res.json(rsv[i]);
-      }
-    }
-  
-    return res.json(false);
-  });
-  
-
-app.post("/rsv" , function(req,res){
-
-    var newRsv = req.body;
-
-    newRsv.name.replace(/\s+/g, "").toLowerCase();
-
-    console.log(newRsv);
-
-    rsv.push(newRsv);
-
-    res.json(newRsv);
-})
 
 
+app.use('/', routes);
 // Starts the server to begin listening
 // =============================================================
 app.listen(PORT, function() {
